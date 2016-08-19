288ac1e09028e82afeb7d9a18a28a8d981d8b9bd
valarsu <valar_su@163.com>
2016-08-19 14:08:17 +0800

    实现用户和文章详情页面

--- models/post.js
+++ models/post.js
@@ -55,7 +55,7 @@ Post.prototype.save = function (callback) {
 };
 
 //读取文章及其相关信息
-Post.get = function (name, callback) {
+Post.getAll = function (name, callback) {
     //打开数据库
     mongodb.open(function (err, db) {
         if(err) {
@@ -88,6 +88,39 @@ Post.get = function (name, callback) {
         })
     });
 };
+
+//获取一篇文章
+Post.getOne = function (name, day, title, callback) {
+    //打开数据库
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取posts集合
+        db.collection('posts', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //根据用户名、发表日期及文章名进行查询
+            collection.findOne({
+                'name': name,
+                'time.day': day,
+                'title': title
+            }, function (err, doc) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                //解析markdown为html
+                doc.post = markdown.toHTML(doc.post);
+                callback(null, doc);//返回查询的第一篇文章
+
+            });
+        });
+    });
+};
+
 
 
 module.exports = Post;
