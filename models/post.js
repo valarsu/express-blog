2ce0b11e21de21888112869c0a7c523d3abaa015
valarsu <valar_su@163.com>
2016-08-29 18:02:21 +0800

    添加转载和转载统计

--- models/post.js
+++ models/post.js
@@ -9,7 +9,7 @@ function Post(name, head, title, tags, post) {
     this.head = head;
     this.title = title;
     this.tags = tags;
-    this.post = post
+    this.post = post;
 }
 
 //存储一篇文章及其相关信息
@@ -33,6 +33,7 @@ Post.prototype.save = function (callback) {
         tags: this.tags,
         post: this.post,
         comments: [],
+        reprint_info: {},
         pv: 0
     };
     //打开数据库
@@ -213,18 +214,55 @@ Post.remove = function (name, day, title, callback) {
                 return callback(err);
             }
 
-            collection.remove({
+            collection.findOne({
                 'name': name,
                 'time.day': day,
                 'title': title
-            }, {
-                w: 1
-            }, function (err) {
-                mongodb.close();
+            }, function (err, doc) {
                 if (err) {
+                    mongodb.close();
                     return callback();
                 }
-                callback(null);
+                //如果有 reprint_from ,即该文章是转载来的，先保存下来reprint_from
+                var reprint_from = '';
+                if (doc.reprint_info.reprint_from) {
+                    reprint_from = doc.reprint_info.reprint_from;
+                }
+                if (reprint_from != '') {
+                    //更新原文章所在稳当的 reprint_to
+                    collection.update({
+                        'name': reprint_from.name,
+                        'time.day': reprint_from.day,
+                        'title': reprint_from.title
+                    }, {
+                        $pull : {
+                            'reprint_info.reprint_to': {
+                                'name': name,
+                                'day': day,
+                                'title': title
+                            }
+                        }
+                    }, function (err) {
+                        if (err) {
+                            mongodb.close();
+                            return callback(err);
+                        }
+                    });
+                }
+                //删除转载来的文章所在的文档
+                collection.remove({
+                    'name': name,
+                    'time.day': day,
+                    'title': title
+                }, {
+                    w: 1
+                }, function (err) {
+                    mongodb.close();
+                    if (err) {
+                        return callback(err);
+                    }
+                    callback(null);
+                });
             });
         });
     });
@@ -341,6 +379,83 @@ Post.search = function (keyword, callback) {
                 }
                 callback(null, docs);
             });
+        });
+    });
+};
+
+//转载一篇文章
+Post.reprint = function (reprint_from, reprint_to, callback) {
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        db.collection('posts', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //找到被转载的文章的原文档
+            collection.findeOne({
+                'name': reprint_from.name,
+                'time.day': reprint_from.day,
+                'title': reprint_from.title
+            }, function (err, doc) {
+                if (err) {
+                    mongodb.close();
+                    return callback(err);
+                }
+
+                var date = new Date();
+                var time = {
+                    date: date,
+                    year: date.getFullYear(),
+                    month: date.getFullYear() + '-' + (date.getMonth() + 1),
+                    day: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate(),
+                    minute: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' +
+                        date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+                };
+
+                delete doc._id;//注意要删除原来的id
+
+                doc.name = reprint_to.name;
+                doc.head = reprint_to.head;
+                doc.time = time;
+                doc.title = (doc.title.search(/[转载]/) > -1) ? doc.title : '[转载]' + doc.title;
+                doc.comments = [];
+                doc.reprint_info = {'reprint_from': reprint_from};
+                doc.pv = 0;
+
+                //更新被转载的原文档的 reprint_info 内的 reprint_to
+                collection.update({
+                    'name': reprint_from.name,
+                    'time.day': reprint_from.day,
+                    'title': reprint_from.title
+                }, {
+                    $push: {
+                        'reprint_info.reprint_to': {
+                            'name': doc.name,
+                            'day': time.day,
+                            'title': doc.title
+                        }
+                    }
+                }, function (err) {
+                    if (err) {
+                        mongodb.close();
+                        return callback(err);
+                    }
+                });
+
+                //将转载生成的副本修改后存入数据库，并返回存储后的文档
+                collection.insert(doc, {
+                    safe: true
+                }, function (err, post) {
+                    mongodb.close();
+                    if (err) {
+                        return callback(err);
+                    }
+                    callback(err, post[0]);
+                });
+            })
         });
     });
 };
--- routes/index.js
+++ routes/index.js
@@ -283,7 +283,31 @@ module.exports = function(app) {
             res.redirect('/');
         });
     });
-    
+
+    app.get('/reprint/:name/:day/:title', checkLogin);
+    app.get('/reprint/:name/:day/:title', function (req, res) {
+        Post.edit(req.params.name, req.params.day, req.params.title, function (err, post) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect(back);
+            }
+            var currentUser = req.session.user,
+                reprint_from = {name: post.name, day: post.time.day, title: post.title},
+                reprint_to = {name: currentUser.name, head: currentUser.head};
+            Post.reprint(reprint_from, reprint_to, function (err, post) {
+                if (err) {
+                    req.flash('error', err);
+                    return res.redirect('back');
+                }
+                req.flash('success', '转载成功!');
+                var url = encodeURI('/u' + post.name + '/' + post.time.day + '/' + post.title);
+                //跳转到转载后的文章页面
+                res.redirect(url);
+            });
+        });
+    });
+
+
     app.get('/archive', function (req, res) {
         Post.getArchive(function (err, posts) {
             if (err) {
